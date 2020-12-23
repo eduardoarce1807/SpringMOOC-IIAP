45acb3cac4298f6b50911223840c7f94eb7cc364
eduardoarce1807 <64025616+eduardoarce1807@users.noreply.github.com>
2020-12-23 06:45:50 -0500

    Add files via upload

---
+++ src/main/java/com/MOOCIIAP/demo/MoocIiapApplication.java
@@ -0,0 +1,13 @@
+package com.MOOCIIAP.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocIiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoocIiapApplication.class, args);
+	}
+
+}
